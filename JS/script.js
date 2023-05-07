5fccb7bb32da1404de88235f7741e07c0cf72edd
its-your-invi <wriddhammandal1307@gmail.com>
2023-05-07 18:32:15 +0530

    Additional Changes

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,22 @@
-const personalityDivs = document.querySelectorAll('.personality');
+const cardDivs = document.querySelectorAll('.card');
 
-  personalityDivs.forEach(personalityDiv => {
-  const liElements = personalityDiv.querySelectorAll('li');
+  cardDivs.forEach(cardDiv => {
+  const liElements = cardDiv.querySelectorAll('li');
   const liCount = liElements.length;
-  const ulElements = personalityDiv.querySelectorAll('ul');
-
+  const ulElements = cardDiv.querySelectorAll('ul');
+  const content = cardDiv.querySelectorAll('.content');
+  const petImg = cardDiv.querySelectorAll('img');
   if (liCount > 3) {
     ulElements.forEach(ulElement => {
       ulElement.style.cssText = "height: 90px; overflow-y: scroll; padding-right: 10px;";
     });
+  }
+  else if (liCount < 3){
+    petImg.forEach(pet => {
+      pet.style.cssText = "position: absolute; top:0;" 
+    })
+    content.forEach(text => {
+      text.style.cssText = "position: absolute; top:36%;" 
+    })
   }
 });
