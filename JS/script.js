62c7cc85fad5c15d2425f655a247d7a95e3151b6
Rik <wriddhammandal1307@gmail.com>
2023-05-09 04:24:35 +0530

    Bugs are fixed (#51)
    * Card Height Fixed
    
    * Additional Changes
    
    * Bugs are fixed

--- JS/script.js
+++ JS/script.js
@@ -4,19 +4,10 @@ const cardDivs = document.querySelectorAll('.card');
   const liElements = cardDiv.querySelectorAll('li');
   const liCount = liElements.length;
   const ulElements = cardDiv.querySelectorAll('ul');
-  const content = cardDiv.querySelectorAll('.content');
-  const petImg = cardDiv.querySelectorAll('img');
+
   if (liCount > 3) {
     ulElements.forEach(ulElement => {
       ulElement.style.cssText = "height: 90px; overflow-y: scroll; padding-right: 10px;";
     });
-  }
-  else if (liCount < 3){
-    petImg.forEach(pet => {
-      pet.style.cssText = "position: absolute; top:0;" 
-    })
-    content.forEach(text => {
-      text.style.cssText = "position: absolute; top:36%;" 
-    })
   }
 });
