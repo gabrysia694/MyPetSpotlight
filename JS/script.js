67437dac4b518ced55050ad4426536ebf6e3e768
Aniket Shitole <aniketshitole565@gmail.com>
2023-05-15 23:02:07 +0530

    Added back to top button with Icon

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,12 @@
 const cardDivs = document.querySelectorAll('.card');
+var backToTopBtn = document.getElementById('back-to-top-btn');
+
+backToTopBtn.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
 
   cardDivs.forEach(cardDiv => {
   const liElements = cardDiv.querySelectorAll('li');
@@ -9,5 +17,14 @@ const cardDivs = document.querySelectorAll('.card');
     ulElements.forEach(ulElement => {
       ulElement.style.cssText = "height: 90px; overflow-y: scroll; padding-right: 10px;";
     });
+  }
+});
+
+window.addEventListener('scroll', function() {
+  var backToTopBtn = document.getElementById('back-to-top-btn');
+  if (window.pageYOffset > 20) {
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none';
   }
 });
